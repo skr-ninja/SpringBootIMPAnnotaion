d9ab7a441e3edfff2ac1640f67ae700c305c23f3
Sunil <rathoursunil1@gmail.com>
2022-09-30 19:17:32 +0530

    intial commit

---
+++ src/main/java/com/springboot/annotation/springbootinterviewannotation/SpringBootInterviewAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.annotation.springbootinterviewannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInterviewAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootInterviewAnnotationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/annotation/springbootinterviewannotation/SpringBootInterviewAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.annotation.springbootinterviewannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInterviewAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
